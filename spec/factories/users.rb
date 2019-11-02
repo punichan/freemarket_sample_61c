24c59c1659d2f7ce59eec4e72dd13db38825f779
市川真巳 <masamikai4580@gmail.com>
2019-11-02 18:49:06 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,9 +10,11 @@ FactoryBot.define do
     first_name_kana       {"aaa"}
     last_name_kana        {"a"}
     phone_number          {"00000000000"}
-    birth_year_on         {"1111"}
-    birth_month_on        {"00"}
-    birth_day_on          {"00"}
+    birth_year_on         {20000101}
+    birth_month_on        {20000101}
+    birth_day_on          {20000101}
+    point                 {"0"}
+
   end
 
 end
