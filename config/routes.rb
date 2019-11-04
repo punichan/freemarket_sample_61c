6f76dc6fb956a143be438672aa7803d864b70c85
condraft <condraft64@gmail.com>
2019-11-04 13:24:32 +0900

    UserViewFileMove

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items
   resources :users, only: [:show]
+  resources :signup do
+    collection do
+      get 'new1st'
+      get 'new2nd'
+      get 'new3rd'
+      get 'new4th'
+      get 'new5th'
+    end
+  end
 end
