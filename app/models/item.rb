3a0784a871743cfa41402d98ddc93ff54dcb235b
23matsunamine23 <noname0323namine@gmail.com>
2019-11-09 12:49:37 +0900

    Update-item.rb2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
-  
   belongs_to :user
   belongs_to :category
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_way
