6f1a8fa3b9d015d41e12e47348d1de819ed6e378
condraft <condraft64@gmail.com>
2019-11-14 01:46:54 +0900

    fix2

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", optional: true
   belongs_to :saler, class_name: "User"
   belongs_to :category
-  belongs_to :brand
+  belongs_to :brand, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_way
