3799e723c3854fb34875f55014b3377230671baa
condraft <condraft64@gmail.com>
2019-11-13 12:38:33 +0900

    fixroutes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  root to: 'items#new'
+  root to: 'items#index'
   resources :items do
     collection do
       get 'buycheck'
