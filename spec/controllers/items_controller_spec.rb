440d377e61ef7db1a09b21999e45083b7269ac9d
市川真巳 <masamikai4580@gmail.com>
2019-11-18 17:33:46 +0900

    komit

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,20 +1,9 @@
 require 'rails_helper'
 
-# describe ItemsController do
-  
-# end
+describe TweetsController do
+  describe 'GET #new' do
+    it "renders the :new template" do
+    end
+  end
 
-
-describe ItemsController do
-  describe 'delete #destroy' do
-        it "deletes the item" do
-          item = create(:item)
-          expect{
-            delete :destroy, id: item
-          }.to change(Item,:count).by(-1)
-        end
-      end
-      it 'redirects the :root' do
-        expect(response).to redirect_to(root_path)
-      end
 end
