9bad35ff610d6c68edfda53ff57692b3430443e1
arinkocom <aricom0404@gmail.com>
2019-11-18 19:24:51 +0900

    factorybot-test

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
+  
   factory :item do
-    
+    name                 {“item”}
+    item_description     {“texttexttexttexttext”}
+    price                {“1000”}
+    brand_id             {“1"}
+    saler_id             {“1”}
+    category_id          {“1"}
+    shoes_size_id        {“1”}
+    condition_id         {“1"}
+    delivery_way_id      {“1”}
+    delivery_burden_id   {“1"}
+    prefecture_id        {“1”}
+    shipment_days_id     {“1"}
+    buyer_id             {“2”}
   end
 end
