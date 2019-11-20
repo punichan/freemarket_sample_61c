702e9f1e33a68c7e231101814567273f63390a8e
arinkocom <aricom0404@gmail.com>
2019-11-20 21:11:08 +0900

    drop-down-menu-news.js

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -15,4 +15,23 @@ $(function(){
   $(".header__contents__right__todo__box").mouseleave(function(){
     $(".header__contents__right__todo__box__k").remove()
   })
+
+
+  $(".header__contents__right__notice__box").mouseover(function(){
+    html = `
+    <div class="header__contents__right__notice__box__news">
+<i class="news-img">
+<img width="55" height="55" src="https://static.mercdn.net/images/mercari_profile.png">
+<i class="news-text">
+  事務局から個別メッセージ
+</i>
+</i>
+</div>
+    `
+    $(".header__contents__right__notice__box").append(html)
+  })
+  $(".header__contents__right__notice__box").mouseleave(function(){
+    $(".header__contents__right__notice__box__news").remove()
+  })
+
 })
