be4df6a0194fe2c79198ea25cb35a5792ea647a2
condraft <condraft64@gmail.com>
2019-11-22 12:37:24 +0900

    jsnow

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -15,6 +15,18 @@ $(function(){
                       </div>`;
     $('.form-group-cat').append(childSelectHtml);
   }
+  function appendGrandchildrenBox(insertHTML){
+    var grandchildSelectHtml = '';
+    grandchildSelectHtml = `<div class='childcat' id= 'grandchildren_cat'>
+                        <div class='childcat-box'>
+                          <select class="childcat-select" id="grandchild_category" name="category_id">
+                            <option value="---" data-category="---">---</option>
+                            ${insertHTML}
+                          <select>
+                        </div>
+                      </div>`;
+    $('.form-group-cat').append(grandchildSelectHtml);
+  }
   $('#item_category_id').on('change', function(){
     console.log("change");
     console.log(this);
@@ -29,14 +41,42 @@ $(function(){
       })
       .done(function(children){
         console.log(children)
-        $('#children_wrapper').remove();
-        $('#grandchildren_wrapper').remove();
+        $('#children_cat').remove();
+        $('#grandchildren_cat').remove();
         var insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
         });
         appendChildrenBox(insertHTML);
       })
+    }else{
+      $('#children_cat').remove();
+      $('#grandchildren_wrapper').remove();
+    }
+  });
+  $(document).on('change','.childcat-select', function(){
+    console.log("change");
+    console.log(this);
+    var childId = $('.childcat-select').val();
+    console.log(childId);
+    if (childId != "---"){
+      $.ajax({
+        url: 'get_category_grandchildren',
+        type: 'GET',
+        data: { child_id: childId},
+        dataType: 'json'
+      })
+      .done(function(grandchildren){
+        console.log(grandchildren)
+        $('#grandchildren_cat').remove();
+        var insertHTML = '';
+        grandchildren.forEach(function(grandchild){
+          insertHTML += appendOption(grandchild);
+        });
+        appendGrandchildrenBox(insertHTML);
+      })
+    }else{
+      $('#grandchildren_wrapper').remove();
     }
   });
 });
