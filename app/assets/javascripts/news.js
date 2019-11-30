5a47801cab1cb32a68c553815d9beee39049f68e
arinkocom <aricom0404@gmail.com>
2019-12-01 08:04:17 +0900

    add-drop-down-menu

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -34,4 +34,25 @@ $(function(){
     $(".header__contents__right__notice__box__news").remove()
   })
 
+//   $(".header__contents__right__ava__box__mypage").mouseover(function(){
+//   html=`
+//   <div class="header__contents__right__ava__box__mypage__top">
+// <img width="60" height="60" class="mypage-img" src="https://static.mercdn.net/images/member_photo_noimage_thumb.png">
+// </div>
+// <ul class="list-mypage-tab">
+// <i class="name">test</i>
+// 評価：０ 出品数：２
+// <li class="list-tab-p1">売上金</li>
+// <li class="list-tab-p2">ポイント</li>
+// <li class="list-tab">いいね！一覧</li>
+// <li class="list-tab">出品した商品-出品中</li>
+// <li class="list-tab">出品した商品-取引中</li>
+// <li class="list-tab">出品した商品-売却済み</li>
+// <li class="list-tab">購入した商品-取引中</li>
+// <li class="list-tab">購入した商品-過去の取引</li>
+// </ul>
+// </div>
+//   `
+//   $(".header__contents__right__ava__box__mypage").append(html) 
+// })
 })
